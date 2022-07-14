a2958b27fdd51a406f1b1fdbf941d6eee3a9474f
Lucas Bianchi <lucasbianchi92@gmail.com>
2022-07-14 14:31:42 -0300

    gitignore

--- controllers/products.js
+++ controllers/products.js
@@ -1,19 +1,38 @@
 const Products = require('../models/product');
 
+//Función para testear y ejemplificar find sort select
 const getProductsStatic = async (req, res) => {
-    const products = await Products.find({})
+    const products = await Products.find({}).sort('name price') //Sort lleva espacio entre parametros
     res.status(200).json({ products, nbHits: products.length })
 };
 
+//Buscar según categoría, ordenar y seleccionar.
 const getProducts = async (req, res) => {
-    const { featured, company } = req.query
+    const { featured, company, name, sort, fields } = req.query
     const queryObject = {}
-
+    //Find
     if(featured) {
         queryObject.featured = featured === 'true'? true : false
     }
     if(company) {
         queryObject.company = company
+    }
+    if(name) {
+        queryObject.name = { $regex: name, $options: 'i'}
+    }
+    //Sort
+    let result = Products.find(queryObject)
+    if(sort) {
+        const sortList = sort.split(',').join(' ') //Se formatea , por espacio
+        result = result.sort(sortList)
+    }
+    else {
+        result = result.sort('createdAt') //Por default sortear por Date
+    }
+    //Select
+    if(fields) {
+        const fieldsList = fields.split(',').join(' ')
+        result = result.select(fieldsList)
     }
     const products = await Products.find(queryObject)
     res.status(200).json({ products, nbHits: products.length })
